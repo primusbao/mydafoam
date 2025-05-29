117d113e93496d47520777edd338ed5af295af84
chun bao <128784568+primusbao@users.noreply.github.com>
2025-05-29 19:40:44 +0800

    Update DASpalartAllmarasFv3FieldInversion.C

--- src/adjoint/DAModel/DATurbulenceModel/DASpalartAllmarasFv3FieldInversion.C
+++ src/adjoint/DAModel/DATurbulenceModel/DASpalartAllmarasFv3FieldInversion.C
@@ -333,6 +333,17 @@ tmp<volScalarField> DASpalartAllmarasFv3FieldInversion::f3() const
     );
 }
 
+tmp<volScalarField> DASpalartAllmarasFv3FieldInversion::f4(
+    const volScalarField& Stilda) const
+{  
+
+    return
+    (
+        (mag(Cw1_*fw_*sqr(nuTilda_/y_))-scalar(5)*mag(Cb1_*Stilda*nuTilda_))/ max ((scalar(5)*mag(Cb1_*Stilda*nuTilda_) + mag(Cw1_*fw_*sqr(nuTilda_/y_))),
+                                          dimensionedScalar(dimensionSet(0,2,-2,0,0,0,0), 1e-6))
+    );
+}
+
 tmp<volScalarField> DASpalartAllmarasFv3FieldInversion::DnuTildaEff() const
 {
     return tmp<volScalarField>(
